37345e174cc0212c7872fd9b0564a6c3a06a4335
markelioh <keli.hoxha@hotmail.com>
2025-05-16 09:54:53 +0100

    Update index.tsx

--- edge-middleware/basic-auth-password/pages/index.tsx
+++ edge-middleware/basic-auth-password/pages/index.tsx
@@ -7,8 +7,8 @@ export default function Index() {
         Password-protected page demo
       </Text>
       <List>
-        <li>Username: 4dmin</li>
-        <li>Password: testpwd123</li>
+        <li>Username: admin</li>
+        <li>Password: admin1234</li>
       </List>
     </Page>
   )
